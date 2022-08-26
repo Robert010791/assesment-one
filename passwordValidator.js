82b4b661a865d5c6d30cc9b368cee59b2063a036
Robert S Robbins <robertrobbins321@msn.com>
2022-08-26 13:29:39 -0600

    added comments explaining the rules of the password validator as directed by assessment instructions

--- passwordValidator.js
+++ passwordValidator.js
@@ -9,16 +9,27 @@ const reader = readline.createInterface({
 reader.question(
   'Welcome to the password validator! Please enter your password:',
   function (input) {
+    //this line checks if the password is less than 10 characters long
     if (input.length < 10) {
       console.log('Password is too short');
+
+      //this line checks if the password is over 20 characters long
     } else if (input.length > 20) {
       console.log('Password is too long');
+
+      //this line checks for a number
     } else if (/\d/.test(input) === false) {
       console.log('Password requires at least one number');
+
+      //this line check for a lowercase letter
     } else if (/[a-z]/.test(input) === false) {
       console.log('Password requires at least one letter');
+
+      //this line checks for a capital letter
     } else if (/[A-Z]/.test(input) === false) {
       console.log('Password requires at least one uppercase letter');
+
+      //this line logs an accepted password^^
     } else console.log('Password is accepted');
 
     // This line closes the connection to the command line interface.
